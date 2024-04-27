a0cf687aa229da6b28080f1c2e9106018043f459
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-04-27 14:49:47 +0700

    Done Buoi 11

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -4,17 +4,53 @@ import { useParams } from "react-router-dom";
 
 import { fetchFromAPI, getSearchVideo } from "../utils/fetchFromAPI";
 import { Videos } from "./";
+import axios from "axios";
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState(null);
   const { searchTerm } = useParams();
 
   useEffect(() => {
-    getSearchVideo(searchTerm)
+    getSearchVideo(searchTerm).then((result) => {
+      setVideos(result);
+    });
+
+    let query = {
+      query: `query {getVideo {
+        video_id
+        video_name
+        thumbnail
+        description
+        views
+        source
+        user_id
+        type_id
+        users {
+          user_id
+          full_name
+          email
+          avatar
+          pass_word
+          face_app_id
+          role
+          refresh_token
+        }
+        video_type {
+          type_id
+          type_name
+          icon
+        }
+      }}`,
+    };
+
+    axios
+      .post("http://localhost:8080/api", query)
       .then((result) => {
-        setVideos(result);
+        console.log(result);
       })
-      .catch((err) => {});
+      .catch((error) => {
+        console.log(error);
+      });
   }, [searchTerm]);
 
   return (
